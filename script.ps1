a629ba2e75cad2c880e459a882ff29aad897d568
gobalakrishnaan <110080184+gobalakrishnaan@users.noreply.github.com>
2023-06-10 17:06:13 +0530

    First time added one file

---
+++ script.ps1
@@ -0,0 +1,37 @@
+﻿$serviceName = 'CORRELOG MESSAGE'
+
+
+$Servers = Get-Content -path C:\prakash1.txt
+
+
+foreach($server in $servers)
+{
+
+ 
+
+If ((Get-Service $serviceName -ComputerName $server -ErrorAction SilentlyContinue).Status -eq 'Running' ) {
+
+ 
+
+        Write-Host "$server - $serviceName RUNNING" -ForegroundColor Green -ErrorAction SilentlyContinue
+
+ 
+
+    } Elseif ((Get-Service $serviceName -ComputerName $server  -ErrorAction SilentlyContinue).Status -eq 'Stopped') {
+
+ 
+
+        Write-Host "$server - $serviceName NOT RUNNING"-ForegroundColor Red -ErrorAction SilentlyContinue
+
+ 
+
+    } Else {
+
+ 
+
+    Write-Host "$server - NOT INSTALLED / UNREACHABLE" -ForegroundColor Yellow -ErrorAction SilentlyContinue
+
+ 
+
+}
+}
